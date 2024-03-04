3c01ff79215f696cd16d75e427ad2471308f17da
Jamile Cruz <130008726+milecruz26@users.noreply.github.com>
2024-03-04 16:08:09 -0300

    feat: adicionado formas laranja e roxa ao fundo da tela de login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import Image from "next/image";
 import logo from "../assets/logo-cuca.png";
+import orange from "../assets/elipse-orange.png";
+import purple from "../assets/elipse-purple.png";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-evenly p-6">
-      {/* <Image/> */}
+    <main className="flex min-h-screen flex-col items-center justify-evenly p-6 relative">
+      <Image
+        src={orange}
+        alt="elípse laranja"
+        width={290}
+        className="absolute top-0 right-0 -z-10 w-56"
+      />
       <Image src={logo} alt="logo de um jacaré" />
       <p className="font-sans text-sm text-neutral-700 font-bold text-center w-[248px]">
         Insira as informações abaixo para efetuar o cadastro
@@ -28,7 +35,12 @@ export default function Home() {
         <strong className="underline underline-offset-1">Cadastre-se</strong>
       </p>
 
-      {/* <Image/> */}
+      <Image
+        src={purple}
+        alt="elipse roxa"
+        width={290}
+        className="absolute bottom-0 left-0 -z-10 w-48"
+      />
     </main>
   );
 }
