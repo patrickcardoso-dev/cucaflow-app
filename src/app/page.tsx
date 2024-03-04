47e7c7c7f914a4bc375a46cfcdfdf644d09d6e8a
Tayanna Amorim <amorim.tayanna@gmail.com>
2024-03-04 18:23:38 -0300

    adjustment: correção width lg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,7 @@ import { Label } from "@radix-ui/react-label";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1>Código</h1>
-{/*       <form className="flex flex-col gap-3" >
+      <form className="flex flex-col gap-3" >
         <div>
           <Label htmlFor='email'>E-mail</Label>
           <Input id="email" type="email" placeholder="Ex: email@gmail.com"/>
@@ -23,7 +22,7 @@ export default function Home() {
         <Button variant='orangeSecond'>Orange Second</Button>
         <Button variant='purple'>Purple</Button>
         <Button variant='purpleSecond'>Purple Second</Button>
-      </form> */}
+      </form>
     </main>
   );
 }
