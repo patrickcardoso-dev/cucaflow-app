bc12f0f36924bb8219a6574efd6b9e10457c7f08
Tayanna Amorim <amorim.tayanna@gmail.com>
2024-03-04 17:21:48 -0300

    feature: Criando lógica de mostrar senha e ocultar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 'use client'
+import SvgComponent from "@/components/icon/eyeOpen";
+import { PasswordInput } from "@/components/password-input";
 import { Input } from "@/components/ui/input";
 import { Label } from "@radix-ui/react-label";
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <form className="flex flex-col gap-3" >
         <div>
           <Label htmlFor='email'>E-mail</Label>
@@ -13,7 +15,7 @@ export default function Home() {
         </div>
         <div>
           <Label htmlFor='password'>Senha</Label>
-          <Input id="password" placeholder="Insira sua senha" type="password"/>
+          <PasswordInput id="password" autoComplete="Digite sua senha"/>
         </div>
       </form>
     </main>
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -6,12 +6,12 @@ export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type,  ...props }, ref) => {
     return (
       <input
         type={type}
         className={cn(
-          "flex h-10 w-80 rounded border border-neutras-gray100 bg-neutras-gray200 pl-2 text-sm",
+          "flex h-10 w-80 rounded-xl border border-neutras-gray100 bg-neutras-gray200 pl-2 text-sm",
           className
         )}
         ref={ref}
