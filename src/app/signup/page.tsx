7689fad7c53eea52407a7f43d4057c30fd00836c
João Campos <joaon.c.jv@gmail.com>
2024-03-05 13:40:18 -0300

    feat: added the components in SignUp and some extras stylizations

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,74 +1,99 @@
 import Image from "next/image";
-import CucaflowLogo from "../../assets/logo_cucaflow-03.png"
+import cucaflowLogo from "../../assets/logo_cucaflow-03.png";
+import ellipse3 from "../../assets/ellipse3.png";
+import ellipse2 from "../../assets/ellipse2.png";
+import {Input} from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
 
 export default function SignUp() {
     
     return (
-        <main className="bg-[#FAFAFA] w-screen h-screen">
-            <div className="flex justify-center">
+        <main className="bg-[#FAFAFA] w-screen h-screen p-[24px]">
+            <Image
+                    src={ellipse3}
+                    alt="Ellipe 3"
+                    className="absolute top-0 left-0"
+                />
+            <div className="flex justify-center items-center">
                 <Image
-                    src={CucaflowLogo}
+                    src={cucaflowLogo}
                     alt="Cucaflow logo"
                 />
+                
             </div>
-                <div className="flex flex-col justify-center items-center">
-                    <h1 className="font-bold color text-3xl text-purple-800 cursor-pointer">Bem-vindo(a)</h1>
-                    <p className="w-56 text-center text-sm text-gray-600 font-bold m-4">Insira as informações abaixo para efetuar o cadastro</p>
+            <div className="flex flex-col justify-center items-center">
+                <h1 className="font-extrabold text-[32px] text-primary-purple100">Bem-vindo(a)</h1>
+                <p className="w-[248px] text-center text-[16px] text-neutras-bgBlack font-normal opacity-70 m-2 leading-[20px]">Insira as informações abaixo para efetuar o cadastro</p>
+            </div>
+
+            <form className="flex flex-col justify-center items-center gap-5">
+                <div className="flex flex-col w-[320px]">
+                    <Label htmlFor="name">
+                        Nome de usuário
+                    </Label>
+                    <Input
+                        id="name"
+                        type="text"
+                        placeholder="Ex. cucaflow"
+                        className="w-[312px] mb-4"
+                    />
+                    <Label htmlFor="email">
+                        E-mail
+                    </Label>
+                    <Input
+                        id="name"
+                        type="email"
+                        placeholder="Ex. cucaflow@gmail.com"
+                        className="w-[312px] mb-4"
+                    />
+                
+                    <Label htmlFor="password">
+                        Senha
+                    </Label>
+                    <Input
+                        id="password"
+                        type="password"
+                        placeholder="Insira sua senha"
+                        className="w-[312px] mb-4"
+                    />
+                     <Label htmlFor="password">
+                        Confirmar senha
+                    </Label>
+                    <Input
+                        id="password"
+                        type="password"
+                        placeholder="Insira sua senha"
+                        className="w-[312px] mb-4"
+                    />
                 </div>
-                <form className="flex flex-col justify-center items-center gap-5">
-                    <div className="flex flex-col w-[320px]">
-                        <label htmlFor="user_name" className="text-black text-sm mb-1">Nome de usuário:</label>
-                        <input
-                            type="text"
-                            placeholder="Ex: Seu Nome"
-                            name="user_name"
-                            id="user_name"
-                            className="rounded-lg border-gray-500 border h-[42px] w-[315px] placeholder-slate-500"
-                        />
-                        <label htmlFor="user_email" className="text-black text-sm mb-1">E-mail</label>
-                        <input
-                            type="email"
-                            placeholder="Ex: email@gmail.com"
-                            name="user_email"
-                        id="user_email"
-                        className="rounded-lg border-gray-500 border h-[42px] w-[315px] mb-1 placeholder-slate-500"
-                        />
-                    </div>
-                    <div className="flex flex-col w-[320px]">
-                        <label htmlFor="user_pass" className="text-black text-sm mb-1">Senha</label>
-                        <input
-                            type="password"
-                            placeholder="Insira sua senha"
-                            name="user_pass"
-                            id="user_pass"
-                            className="rounded-lg border-gray-500 border h-[42px] w-[315px] mb-1 placeholder-slate-500"
-                        />
-                        <label htmlFor="user_confirm_pass" className="text-black text-sm mb-1">Confirmar Senha</label>
-                        <input
-                            type="text"
-                            placeholder="Insira sua senha"
-                            name="user_confirm_pass"
-                            id="user_confirm_pass"
-                            className="rounded-lg border-gray-500 border h-[42px] w-[315px] mb-1 placeholder-slate-500"
-                        />
-                    </div>
-                    <div className="flex text-xs">
-                        <input type="checkbox" className="w-6 h-6 border-purple-700 border-4 mr-2"/>
-                        <p className="text-purple-500 text-xs">Eu aceito os Termos e Política de privacidade</p>
-                    </div>
-                    <div>
-                        <button className="bg-purple-700 w-[312px] h-[42px] mb-2">
-                            <a href="#" className="text-white text-bold text-sm">Cadastrar</a>
-                        </button>
-                    </div>
-                </form>
-                <hr />
+
+                <div className="flex items-center mb-4">
+                    <input
+                        type="checkbox" className=" form-checkbox w-[22px] h-[22px] border-4 border-blue- mr-2" />
+                    <p className="text-[#AF61F2] text-xs">Eu aceito os Termos e Política de privacidade</p>
+                </div>
+                <div>
+                    <Button className="bg-primary-purple100 w-[312px] ">
+                        <p className="text-bold text-sm">Cadastrar</p>
+                    </Button>
+                </div>
+            </form>
+
+            <hr className="text-secondary-orange300 w-56 m-auto mt-6 mb-4"/>
+            
                 <div className="flex justify-center items-center">
-                    <p className="text-purple-700 text-bold text-center text-sm">Já possui conta?</p>
-                <strong>
-                    <a href="#" className="text-purple-700 text-bold text-center text-sm underline ml-2">Faça o Login</a>
-                </strong>
+                    <p className="text-[#49484D] font-bold text-center text-sm">Já possui conta?</p>
+                    <strong>
+                        <a href="#" className="text-[#EE5D21] text-bold text-center text-sm underline ml-1">Faça o Login</a>
+                    </strong>
+                    
                 </div>
+                <Image
+                        src={ellipse2}
+                        alt="Ellipe 2"
+                        className="absolute top-screen right-0"
+                    />
        </main>
     )
 }
