07c2258a4fc4c4664ec4834753a8503f9371a5de
João Campos <joaon.c.jv@gmail.com>
2024-03-08 17:22:16 -0300

    refactor: addings signup files and refactoring the code.

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,105 +1,70 @@
 import Image from "next/image";
 import { manrope } from "./fonts";
-import cucaflowLogo from "../../assets/logo_cucaflow-03.png";
-import ellipse3 from "../../assets/ellipse3.png";
-import ellipse2 from "../../assets/ellipse2.png";
-import { Input } from "@/components/ui/input";
-import { PasswordInput } from "@/components/password-input";
-import { Label } from "@/components/ui/label";
-import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
+import cucaflowLogo from "../../assets/logo_cucaflow.png";
+import ellipseTop from "../../assets/ellipse-top.png";
+import ellipseBottom from "../../assets/ellipse-bottom.png";
+import ellipseTopDesktop from "../../assets/ellipse-top-desktop.png";
+import ellipseBottomDesktop from "../../assets/ellipse-bottom-desktop.png";
+import largeLogo from "../../assets/big-logo-cuca.png";
 import Link from "next/link";
+import { SignUpForm } from "@/components/form/signup";
 
 export default function SignUp() {
     
     return (
-        <main className={`flex flex-col ${manrope.className} bg-neutras-bgWhite h-screen pt-6 pb-6 tablet:justify-center`}>
-            <Image
-                    src={ellipse3}
-                    alt="Ellipe 3"
-                    className="absolute top-0 left-0"
-                />
+        <main className={`${manrope.className} tablet:flex tablet:items-center tablet:justify-center bg-neutra-bgWhite w-screen h-screen p-8`}>
             <div>
+                <Image
+                        src={ellipseTop}
+                        alt="Ellipse Top"
+                        className="block laptop:hidden absolute top-0 left-0"
+                />
+                <Image
+                        src={ellipseTopDesktop}
+                        alt="Ellipse Top"
+                        className="hidden laptop:block absolute laptop:w-52 desktop:w-72 top-0 left-0"
+                />
                 <div className="flex justify-center items-center">
                     <Image
                         src={cucaflowLogo}
                         alt="Cucaflow logo"
+                        className="block laptop:hidden "
                     />
-                
                 </div>
-                <div className="flex flex-col justify-center items-center">
-                    <h1 className="font-extrabold text-3xl text-primary-purple100">Bem-vindo(a)</h1>
-                    <p className="w-[248px] text-center text-sm text-neutras-bgBlack font-normal opacity-70 m-2 leading-5">Insira as informações abaixo para efetuar o cadastro</p>
-                </div>
-            </div>
-
-            <div className="flex flex-col items-center">
-                <form className="flex flex-col justify-center items-center gap-5">
-                    <div className="flex flex-col gap-4">
-                        <div>
-                            <Label htmlFor="name">
-                                Nome de usuário
-                            </Label>
-                            <Input
-                                id="name"
-                                type="text"
-                                placeholder="Ex. cucaflow"
-                            />
-                        </div>
-                        <div>
-                            <Label htmlFor="email">
-                                E-mail
-                            </Label>
-                            <Input
-                                id="name"
-                                type="email"
-                                placeholder="Ex. cucaflow@gmail.com"
-                            />
-                        </div>
-                
-                        <div>
-                            <Label htmlFor="password">
-                                Senha
-                            </Label>
-                            <PasswordInput
-                                id="password"
-                                placeholder="Insira sua senha"
-                            />
-                        </div>
-                        <div>
-                            <Label htmlFor="password-confirm">
-                                Confirmar senha
-                            </Label>
-                            <PasswordInput
-                                id="password-confirm"
-                                placeholder="Insira sua senha"
-                            />
-                        </div>
-                    </div>
-                    <div className="flex items-center">
-                        <Checkbox />
-                        <p className="text-primary-purple400 text-xs pl-2">Eu aceito os Termos e Política de privacidade</p>
-                    </div>
-                
-                    <Button variant="purple" size="default" className="w-[320px] rounded-md">
-                        <p className="text-bold text-sm">Cadastrar</p>
-                    </Button>
-                </form>
-                <hr className="text-secondary-orange300 w-56 m-auto mt-6 mb-4"/>
-                
-                    <div className="flex justify-center items-center pb-12 w-screen">
-                        <p className={`text-neutras-gray300 font-bold text-center text-sm ${manrope.className}`}>Já possui conta?</p>
-                        <strong>
-                        <Link href="/" className="text-secondary-orange400 text-bold text-center text-sm underline ml-1">Faça o Login</Link>
-                        </strong>
+                <div className="flex flex-col laptop:flex-row laptop:justify-between laptop:items-start laptop:w-[1100px] laptop:p-24">
+                    <div className="flex flex-col justify-center items-center">
                         <Image
-                            src={ellipse2}
-                            alt="Ellipe 2"
-                            className="absolute tablet:bottom-0 desktop:overflow-y-hidden top-screen right-0 "
+                            src={largeLogo}
+                            alt="Cucaflow logo"
+                            className="hidden laptop:block laptop:w-[400px] laptop:mt-12"
                         />
+                        <h1 className="font-extrabold text-3xl text-primary-purple100 mt-4">Bem-vindo(a)</h1>
+                    </div>
+                    <div className="flex flex-col justify-center items-center">
+                        <p className="text-center text-sm laptop:text-xl laptop:text-primary-purple100 text-neutras-bgBlack font-normal opacity-70 m-2 leading-5 w-64 laptop:w-80">Insira suas informações abaixo para efetuar o cadastro</p>
+                
+                        <SignUpForm />
+                        <hr className="text-secondary-orange300 w-56 m-auto mt-6 mb-4"/>
+                
+                        <div className="flex justify-center items-center">
+                            <p className={`text-[#49484D] font-bold text-center text-sm ${manrope.className}`}>Já possui conta?</p>
+                            <strong>
+                                <Link href="/" className="text-secondary-orange400 text-bold text-center text-sm underline ml-1">Faça o Login</Link>
+                            </strong>
+                            <Image
+                                src={ellipseBottom}
+                                alt="Ellipe Bottom"
+                                className="block laptop:hidden absolute bottom-screen tablet:bottom-0 right-0"
+                            />
+                            <Image
+                                src={ellipseBottomDesktop}
+                                alt="Ellipe Bottom"
+                                className="hidden laptop:block absolute laptop:w-52 desktop:w-72 bottom-screen bottom-0 right-0"
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
-            
        </main>
     )
 }
---
+++ src/components/form/schema.tsx
@@ -0,0 +1,28 @@
+import { z } from "zod";
+
+const formSchema = z.object({
+    username: z
+        .string()
+        .min(3, {
+    message: "Nome de usuário precisa ter pelo menos 3 caracteres",
+  }),
+    email: z
+        .string()
+        .email({ message: 'E-mail inválido' }),
+    password: z
+        .string({
+    required_error: 'Senha é obrigatória'
+        })
+        .min(6, { message: 'A senha precisa ter pelo menos 6 caracteres' })
+        .max(10, { message: 'A senha não pode ultrapassar 10 caracteres' })
+    ,
+    confirmPassword: z
+        .string({
+    required_error: 'Confirmação da senha é obrigatória'
+        }),
+}).refine(data => data.password === data.confirmPassword, {
+    message: 'As senhas não coincidem',
+    path: ['confirmPassword']
+});
+
+export default formSchema
