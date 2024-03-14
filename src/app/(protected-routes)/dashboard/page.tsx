d0b181ae5f4cc7e147a8d8074910bad90b2d76c8
Tayanna Amorim <amorim.tayanna@gmail.com>
2024-03-14 15:22:16 -0300

    feat: Criando uma mensagem de carregamento

--- src/app/(protected-routes)/dashboard/page.tsx
+++ src/app/(protected-routes)/dashboard/page.tsx
@@ -11,11 +11,14 @@ export  default  function Dashboard() {
   }
   
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-6 p-6 relative overflow-hidden laptop:flex-row">
+    <main className="flex min-h-screen items-center justify-center gap-6 p-6 relative overflow-hidden laptop:flex-row">
+      {user ? (<div className="flex flex-col justify-center items-center gap-4">
       <h1>{user?.name}</h1>
       <h2>{user?.email}</h2>
       <img src={user?.image ?? ''} alt="" />
       <Button onClick={sair}>Sair</Button>
+      </div>) : (<div>Carregando...</div>)}
+      
     </main>
   )
 }
