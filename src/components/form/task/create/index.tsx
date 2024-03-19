6da517f558c564ac58b4291ac05a8ec9ed817a2e
Tayanna Amorim <amorim.tayanna@gmail.com>
2024-03-19 17:03:12 -0300

    Feat: Criaão do componente container e editar tarefa

--- src/components/form/task/create/index.tsx
+++ src/components/form/task/create/index.tsx
@@ -15,10 +15,11 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { Dispatch, SetStateAction } from "react";
+import ContainerTask from "../container";
 
 type  formSchemaTodoData= z.infer<typeof schemaToDo>;
 
-function CreateTask({setShowModal}: {setShowModal: Dispatch<SetStateAction<Boolean>>} ) {
+function CreateTask({setShowAddTasks}: {setShowAddTasks: Dispatch<SetStateAction<Boolean>>} ) {
   const form = useForm<formSchemaTodoData>({
     resolver: zodResolver(schemaToDo), 
     defaultValues: {
@@ -34,71 +35,73 @@ function CreateTask({setShowModal}: {setShowModal: Dispatch<SetStateAction<Boole
   }
 
   return (
-    <Form {...form}>
-      <div>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="flex flex-col h-full"
-        >
-          <h1 className="text-center text-xl text-neutras-bgBlack">Adicionar Tarefa</h1>
-          <FormField
-            control={form.control}
-            name="time"
-            render={({ field }) => (
-              <FormItem className="flex items-center justify-between py-5">
-                <FormLabel
-                  className={`text-sm ${manrope.className} desktop:text-base`}
-                >
-                  Horário
-                </FormLabel>
-                <FormMessage />
-                <FormControl>
-                  <Input   type="time" className="w-24  border-neutras-gray200 text-center flex flex-col justify-center p-1 cursor-pointer" {...field} />
-                </FormControl>
-              </FormItem>
-            )}
-          />
-          <div className="flex flex-col gap-5 mb-4">
+    <ContainerTask>
+      <Form {...form}>
+        <div>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col h-full"
+          >
+            <h1 className="text-center text-xl text-neutras-bgBlack">Adicionar Tarefa</h1>
             <FormField
               control={form.control}
-              name="title"
+              name="time"
               render={({ field }) => (
-                <FormItem className="items-center justify-between">
+                <FormItem className="flex items-center justify-between py-5">
                   <FormLabel
                     className={`text-sm ${manrope.className} desktop:text-base`}
                   >
-                    Título
+                    Horário
                   </FormLabel>
-                  <FormControl>
-                    <Input placeholder="nome da sua tarefa" className="w-full text-sm" {...field} />
-                  </FormControl>
                   <FormMessage />
+                  <FormControl>
+                    <Input   type="time" className="w-24  border-neutras-gray200 text-center flex flex-col justify-center p-1 cursor-pointer" {...field} />
+                  </FormControl>
                 </FormItem>
               )}
             />
-            <FormField
-              control={form.control}
-              name="description"
-              render={({ field }) => (
-                <FormItem className="items-center justify-between">
-                  <FormLabel
-                    className={`text-sm ${manrope.className} desktop:text-base`}
-                  >
-                    Descrição
-                  </FormLabel>
-                  <FormControl>
-                    <Textarea placeholder="descreva sua tarefa" className="w-full text-sm" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          <Button type="submit" className="mb-3">Comfirmar</Button>
-          <Button onClick={() => setShowModal(false)} variant='orangeSecond' type="submit">Cancelar</Button>
-        </form>
-      </div>
-    </Form>
+            <div className="flex flex-col gap-5 mb-4">
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem className="items-center justify-between">
+                    <FormLabel
+                      className={`text-sm ${manrope.className} desktop:text-base`}
+                    >
+                      Título
+                    </FormLabel>
+                    <FormControl>
+                      <Input placeholder="nome da sua tarefa" className="w-full text-sm" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem className="items-center justify-between">
+                    <FormLabel
+                      className={`text-sm ${manrope.className} desktop:text-base`}
+                    >
+                      Descrição
+                    </FormLabel>
+                    <FormControl>
+                      <Textarea placeholder="descreva sua tarefa" className="w-full text-sm" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <Button type="submit" className="mb-3">Comfirmar</Button>
+            <Button onClick={() => setShowAddTasks(false)} variant='orangeSecond' type="submit">Cancelar</Button>
+          </form>
+        </div>
+      </Form>
+    </ContainerTask>
   )
 }
 
