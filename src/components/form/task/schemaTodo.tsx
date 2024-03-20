e598e3410487b2cd0db2d8dfe0cd4feb0e491860
Tayanna Amorim <amorim.tayanna@gmail.com>
2024-03-20 15:59:15 -0300

    feat: Criação de validação de ano do date

--- src/components/form/task/schemaTodo.tsx
+++ src/components/form/task/schemaTodo.tsx
@@ -2,9 +2,15 @@ import {z} from 'zod'
 
 const schemaToDo = z.object({
   time: z.string(),
-  date: z.string(),
+  date: z.string().transform((value, ctx) => new Date(value).getTime()),
   title: z.string().min(4, {message: 'Sua tarefa não pode ficar sem título'}),
   description: z.string(),
-})
+}).refine((data) => {
+  const year = new Date(data.date).getFullYear();
+  return year >= 1900 && year <= 2100;
+}, {
+  message: "Ano inválido",
+  path: ["date"],
+});
 
 export default schemaToDo
