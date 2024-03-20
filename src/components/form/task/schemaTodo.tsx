9bc922c2457cc3be33b6622a26406217e1fafcfd
Tayanna Amorim <amorim.tayanna@gmail.com>
2024-03-20 11:42:23 -0300

    feat: adição do input date

--- src/components/form/task/schemaTodo.tsx
+++ src/components/form/task/schemaTodo.tsx
@@ -2,6 +2,7 @@ import {z} from 'zod'
 
 const schemaToDo = z.object({
   time: z.string(),
+  date: z.string(),
   title: z.string().min(4, {message: 'Sua tarefa não pode ficar sem título'}),
   description: z.string(),
 })
