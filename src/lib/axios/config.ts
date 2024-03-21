09aaa3f17cb41d0c84c35730989f599c9a0ebdba
Tayanna Amorim <amorim.tayanna@gmail.com>
2024-03-21 14:32:47 -0300

    chore: modificando nome do arquivo

--- src/lib/axios/config.ts
+++ src/lib/axios/config.ts
@@ -1,17 +1,15 @@
 import axios from "axios";
 import { cookies } from "next/headers";
 
-const axiosConfig = axios.create({
+export const api = axios.create({
   baseURL: "https://cucaflow-api.cyclic.app/",
   headers: { "Content-Type": "application/json" },
 });
 
-axiosConfig.interceptors.request.use((request) => {
+api.interceptors.request.use((request) => {
   const token = cookies().get("token");
   if (token) {
     request.headers.Authorization = "Bearer " + token;
   }
   return request;
 });
-
-export default axiosConfig;
