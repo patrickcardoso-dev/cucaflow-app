b94b7db9570518f070593b52b327ac80f4c47ac5
João Campos <joaon.c.jv@gmail.com>
2024-04-05 17:01:34 -0300

    fix: change the baseURL of api axios

--- src/lib/axios/config.ts
+++ src/lib/axios/config.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { cookies } from "next/headers";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://cucaflow-api.cyclic.app/",
   timeout: 10000,
   headers: { "Content-Type": "application/json" },
 });
